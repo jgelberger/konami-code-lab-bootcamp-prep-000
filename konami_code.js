3d6550e43a95968fdd75ab68e8a386535dc35265
Joshua Gelberger <joshua_gelberger@alumni.brown.edu>
2018-09-13 04:06:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -17,15 +17,15 @@ function init() {
   
   function onKeyDownHandler(e) {
     
-    const key = event.key;
+    const key = e.key;
     if (key === codes[index]) {
       index++;
     
       if (index === codes.length) {
-        alert('')
+        alert('Congrats! You found the Konami Code!');
       }
     }
- 
+  }
  document.body.addEventListener('keydown', )
  
 }
